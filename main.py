5e271d0894629f0c964ef2e0af8c77f619210d6f
Mocte Sandoval Vargas <mocte.lince@gmail.com>
2026-01-03 11:22:15 -0600

    Recibiendo texto del prompt desde la linea de comandos

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from aiagent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) < 2:
+        print("Necesito un prompt")
+        sys.exit(2)
+    prompt = sys.argv[1]
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=prompt,
+    )
+    print(response.text)
+    if response is None or response.usage_metadata is None:
+        print("Respuesta anormal")
+        return
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
-if __name__ == "__main__":
-    main()
+main()
